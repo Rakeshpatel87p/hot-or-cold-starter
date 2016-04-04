bc413a78236345a1484c57712128f8ec40b25481
Rakesh Patel <emailme@rakeshrpatel.com>
2016-04-04 07:38:52 +0530

    beginnings

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 
+
+// Cleaner way to write this:
+var randomNumber=0;
+getRandomNumber(0, 100);
+console.log(randomNumber);
+
 $(document).ready(function(){
-	
+
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +19,34 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
+  	$("#guessButton").click(function(e){
+  		e.preventDefault();
+  		var userGuess = parseInt(document.getElementById("userGuess").value);
+  		var guesses = parseInt(document.getElementById("count").value);
+  		console.log(guesses);
+  		
+  		if (userGuess == randomNumber){
+  			$("#feedback").text("You Got it!");
+  		}
+
+  		else if(userGuess <= randomNumber + 5 && userGuess>= randomNumber - 5){
+  			$("#feedback").text("Hot, Hot, Hot!");
+  		}
+
+  		else if (userGuess <= randomNumber + 10 && userGuess >= randomNumber - 10){
+  			$("#feedback").text("Warm. Keep Going");
+  		
+  		} else {
+  			$("#feedback").text("Icy cold");
+  		} 		
+  
+  	$("#guessList").append("<li>" + userGuess + "</li>");
+
+  	});
+
 });
 
-
+function getRandomNumber(min, max) {
+    randomNumber = Math.floor(Math.random() * (max - min) + min);
+}
