81157c819dc4e86d7710a2e082749602d404f18b
Rakesh Patel <emailme@rakeshrpatel.com>
2016-04-04 18:16:36 +0530

    updated number update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 // Cleaner way to write this:
 
 var randomNumber = getRandomNumber(0, 100);
-var userGuess=0;
 console.log(randomNumber);
+var userGuess = 0;
+var length = 0;
+
+//Need to add # of guesses: 
+var guesses = parseInt(document.getElementById("count").innerHTML);
+console.log(guesses);
+
 
 $(document).ready(function() {
 
@@ -16,27 +22,26 @@ $(document).ready(function() {
     /*--- Hide information modal box ---*/
     $("a.close").click(function() {
         $(".overlay").fadeOut(1000);
+    
     });
 
 
     $("#guessButton").click(function(e) {
         e.preventDefault();
         guessGame(5, 10);
+        $("#count").text(length);
+        $("#userGuess").val("")
 
     });
 
     $(".new").click(function() {
         getRandomNumber(0, 100);
         console.log(randomNumber);
-
-        // Remove all li
-        $("#guessList li:all").remove();
-        // $("#guessList").empty();
-        $("#feedback").text("Make Your Guess!");
-        // Clear textbar
+        $("#userGuess").val("");
+        $("#guessList li").remove();
         guessGame(5, 10);
-
-        
+        $("#feedback").text("Make Your Guess!");
+        $("#count").text("0");
 
     });
 
@@ -50,34 +55,29 @@ function getRandomNumber(min, max) {
 function guessGame(hotter, hot) {
     userGuess = parseInt(document.getElementById("userGuess").value);
     console.log(userGuess);
-    
-	//Need to add # of guesses: 
-    // var guesses = parseInt(document.getElementById("count").value);
-    // console.log(guesses);
 
-if (userGuess == randomNumber) {
+    if (userGuess == randomNumber) {
         $("#feedback").text("You Got it!");
         appendToList();
-        
-    } else if (userGuess <= randomNumber + hotter && userGuess >= randomNumber - hotter){
+
+    } else if (userGuess <= randomNumber + hotter && userGuess >= randomNumber - hotter) {
         $("#feedback").text("Hot, Hot, Hot!");
         appendToList();
     } else if (userGuess <= randomNumber + hot && userGuess >= randomNumber - hot) {
         $("#feedback").text("Warm. Keep Going");
         appendToList();
 
-	} else if (isNaN(userGuess)) {
-    	$("#feedback").text("Not a number. Try again!");
+    } else if (isNaN(userGuess)) {
+        $("#feedback").text("Not a number. Try again!");
 
     } else {
         $("#feedback").text("Icy cold");
         appendToList();
     }
 
-
-    
 };
 
-function appendToList(){
-	$("#guessList").append("<li>" + userGuess + "</li>");
+function appendToList() {
+    $("#guessList").append("<li>" + userGuess + "</li>");
+    length = $("#guessList > li").length
 };
